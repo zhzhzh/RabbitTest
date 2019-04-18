72cf9fed380043417ea80770f852b5b566717a95
Zhang Jie <jzhang14@paypal.com>
2019-04-18 19:54:50 +0800

    update EmitLogDirect

--- src/main/java/EmitLogDirect.java
+++ src/main/java/EmitLogDirect.java
@@ -1,3 +1,4 @@
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -12,8 +13,8 @@ public class EmitLogDirect {
         factory.setUsername("rabbit");
         factory.setPassword("rabbit12345");
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
 
             String severity = getSeverity(argv);
             String message = getMessage(argv);
@@ -23,12 +24,27 @@ public class EmitLogDirect {
         }
     }
 
-    private static String getSeverity(String[] argv) {
-        return argv[0];
+    private static String getSeverity(String[] strings) {
+        if (strings.length < 1)
+            return "info";
+        return strings[0];
     }
 
-    private static String getMessage(String[] argv) {
-        return argv[1];
+    private static String getMessage(String[] strings) {
+        if (strings.length < 2)
+            return "Hello World!";
+        return joinStrings(strings, " ", 1);
+    }
+
+    private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+        int length = strings.length;
+        if (length == 0) return "";
+        if (length <= startIndex) return "";
+        StringBuilder words = new StringBuilder(strings[startIndex]);
+        for (int i = startIndex + 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
     }
 
 
